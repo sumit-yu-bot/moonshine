7829a0089494a589fa7e3cf97ce9f64038748b88
sumit <118124900+sumit-yu-bot@users.noreply.github.com>
2023-06-12 14:18:42 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -41,15 +41,15 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="❣ sᴜᴩᴩᴏʀᴛ ❣", url=config.SUPPORT_GROUP
+                text="☊ sᴜᴩᴩᴏʀᴛ ☊", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="🥀 ᴍᴀɪɴᴛᴀɪɴᴇʀ 🥀", user_id=OWNER
+                text="🍃 ꜱᴀꜱᴛᴀ ᴏᴡɴᴇʀ 🍃", user_id=OWNER
             )
         ],
         [
             InlineKeyboardButton(
-                text="✨ sᴏᴜʀᴄᴇ ✨", url=config.UPSTREAM_REPO
+                text="▷  sᴏᴜʀᴄᴇ ✨", user_id=OWNER
             )
         ],
      ]
